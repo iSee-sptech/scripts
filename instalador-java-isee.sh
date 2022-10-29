fd7470970fe7d8e985e3094b9a3f7a6bc25ccdbb
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2022-10-29 15:22:15 -0300

    Alterando alguns textos e tempo de sleep

--- instalador-java-isee.sh
+++ instalador-java-isee.sh
@@ -2,24 +2,25 @@
 java -version
 if [ $? -eq 0 ]
     then
-    echo \"java instalado\"
+    echo \"[Java já instalado]\"
     sleep 6
     else
-    echo \"java não instalado\"
+    echo \"[Java ainda não instalado]\"
 
-    echo \"instalado pacote java\"
-        echo \"instalado repositorio\"
+    echo \"[Preparando para instalação...instalando pacote Java...]\"
+        echo \"[instalando repositório...]\"
         sleep 6
         sudo add-apt-repository ppa:webupd8team/java -y
         clear
-        echo \"Atualizando repositorio\"
+        echo \"[[Atualizando repositório...]\"
         sleep 
         sudo apt-get update -y
         clear
 
-        echo \"Instalando Java na versão 11\"
+        echo \"[Instalando Java - versão 11...]\"
         sudo apt-get install default-jre -y
         sleep 10
         clear
-        echo \"java instalado versão 11\"
+        echo \"[Java instalado - versão 11 :)]\"
+        sleep 2
 fi
