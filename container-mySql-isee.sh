40f02cba6e31ab2cc1a5542a8901bdea1914102b
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2022-11-12 10:28:33 -0300

    rollback

--- container-mySql-isee.sh
+++ container-mySql-isee.sh
@@ -30,9 +30,12 @@
             
             sudo su
             docker exec -it ContainerMySql bash
-            mysql -u root -purubu100 -e "
+            mysql -u root -p
+            urubu100
+            sleep 1
             
 use iSee;
+
 create table Usuarios (
 idUsuario int primary key auto_increment,
 nomeUsuario varchar (100),
@@ -48,7 +51,70 @@ imagemPerfilUsuario varchar (255),
 numeroLocalUsuario varchar (6),
 complementoLocalUsuario varchar (25)
 );
-"
+
+create table Maquinas (
+idMaquina int primary key auto_increment,
+serialMaquina varchar(25),
+sistemaOperacionalMaquina varchar(25),
+fabricanteMaquina varchar(25),
+arquiteturaMaquina varchar(25),
+tempoDeAtividade long,
+nomeMaquina varchar (100),
+discoMaquina varchar(10),
+ramMaquina varchar(10),
+processadorMaquina varchar(10),
+cepMaquina char (9) unique,
+numeroMaquina varchar (6),
+pontoDeReferencia varchar (25), 
+imgMaquina varchar (255),
+complementoMaquina varchar (50),
+fkUsuario int,
+foreign key (fkUsuario)
+references Usuarios (idUsuario)
+);
+
+create table Etiqueta (
+idEtiqueta int primary key auto_increment,
+fkMaquina int,
+nomeEtiqueta varchar(50),
+datahoraEtiqueta datetime,
+foreign key (fkMaquina)
+references Maquinas (idMaquina)
+);
+
+create table Historico (
+idHistorico int primary key auto_increment,
+usoRamHistorico varchar(10),
+usoProcessadorHistorico varchar(10),
+usoDiscoHistorico varchar(10),
+dataHoraHistorico datetime default current_timestamp,
+statusHistorico varchar (50),
+logHistorico varchar (255),
+fkMaquinaHistorico int,
+foreign key (fkMaquinaHistorico)
+references Maquinas (idMaquina)
+);
+
+create table Lembrete(
+idLembrete int primary key auto_increment,
+mensagemLembrete varchar (50),
+dataHoraLembrete datetime,
+horarioGeradoLembrete datetime,
+fkUsuario int,
+foreign key (fkUsuario)
+references Usuarios (idUsuario)
+);
+
+create table Alerta (
+idAlerta int primary key auto_increment,
+fkMaquina int,
+componente varchar(10),
+nivelAlerta varchar(10),
+dado varchar(10),
+datahoraAlerta datetime,
+foreign key (fkMaquina)
+references Maquinas (idMaquina)
+);
             
             sleep 3
             exit
